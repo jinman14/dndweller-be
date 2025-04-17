274c6900d9c811b903c1b0746b11b651643b3729
Sebastian McKee <sebmcke@gmail.com>
2025-04-17 16:17:30 -0500

    Fix column name for equipment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_17_195535) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_17_211620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_17_195535) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "type"
+    t.string "equipment_type"
     t.string "damage_dice"
     t.string "damage_type"
     t.string "range"
