236d21b3d1377a4b777b0de582a74f54b7d1d92e
Sebastian McKee <sebmcke@gmail.com>
2025-04-17 16:38:59 -0500

    update skill factory

--- spec/factories/skill_factory.rb
+++ spec/factories/skill_factory.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
     factory :skill do
         name {"Acid-Arrow"}
+        level { 2 }
+        damage_type { "acid" }
+        range { "90 feet" }
+        description { "A shimmering green arrow streaks toward a target within range and bursts in a spray of acid. Make a ranged spell attack against the target. On a hit, the target takes 4d4 acid damage immediately and 2d4 acid damage at the end of its next turn. On a miss, the arrow splashes the target with acid for half as much of the initial damage and no damage at the end of its next turn." }
     end
 end
