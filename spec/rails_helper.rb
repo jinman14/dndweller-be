f9203181eab7f343a2013b3115f89d15bf473daf
Sebastian McKee <sebmcke@gmail.com>
2025-04-17 15:40:48 -0500

    remove allow http request without vcr fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -84,7 +84,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.default_cassette_options = { re_record_interval: 7.days }
   config.configure_rspec_metadata!
-  config.allow_http_connections_when_no_cassette = true
+  # config.allow_http_connections_when_no_cassette = true
   config.before_record do |i|
     i.response.body.force_encoding('UTF-8')
   end
