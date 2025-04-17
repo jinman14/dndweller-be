9003db42c90bd3350b075f682d8318ad4c3c6b97
Sebastian McKee <sebmcke@gmail.com>
2025-04-17 15:40:16 -0500

    Fix faraday config

--- app/gateways/character_gateway.rb
+++ app/gateways/character_gateway.rb
@@ -1,11 +1,13 @@
 class CharacterGateway
     def self.fetch_speed(race)
-       return conn.get("/races/#{race}")["speed"]
+       response = conn.get("/api/2014/races/#{race}")
+       json = JSON.parse(response.body, symbolize_names: true)
+       return json[:speed]
     end
 
     private
 
     def self.conn 
-        Faraday.new(url: "https://5e-bits.github.io/docs/api")
+        Faraday.new(url: "https://www.dnd5eapi.co")
     end
 end
