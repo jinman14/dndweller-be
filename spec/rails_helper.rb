50d3635b4a54ae3b082a0cbe2eef1c20ac89f589
Sebastian McKee <sebmcke@gmail.com>
2025-04-17 15:28:52 -0500

    Add vcr config to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 require 'pry'
+require 'vcr'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -75,5 +76,16 @@ Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
+  config.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
   end
 end
